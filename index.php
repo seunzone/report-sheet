b00d9bd24fa8d4bb39b9e203b0ece6fd9b099cf3
seunzone <darealseun@gmail.com>
2017-07-30 20:44:48 +0100

    front-end

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	 
 	
 		<h1>Enter Students Results Here</h1>
-			<form action="check.php">
+			<form action="displayResult.php">
 				<table class="imagetable">
 					  <tr>
 						<th>Names</th>
@@ -29,7 +29,288 @@
 						<th>Computer Studies</th>
 					  </tr>
 					  <tr>
-						<td>Seun Daramola</td>
+						<td>Femi Johnson</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+					  </tr>
+
+
+					   <tr>
+						<td>Chibuzo Michaels</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+					  </tr>
+
+					   <tr>
+						<td>Musa Philips</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+					  </tr>
+
+					   <tr>
+						<td>Leslie Dzong</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+					  </tr>
+
+					   <tr>
+						<td>Oju Ciroma</td>
 						<td>
 							<input type="number">
 							<select>
