581a52781ba94581818c49538d247c03773b2af8
seunzone <darealseun@gmail.com>
2017-07-30 20:22:57 +0100

    front-end

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# report-sheet
+# school-roster
+Part of our curriculum for teaching school Kids
+Tutors are @seunzone and @thirdwrist
+
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- CSS FILES -->
+<link rel="stylesheet" href="css/main.css"/>
+
+
+</head>
+
+
+<body>
+<hr>
+	
+	<div class="myDiv" align="center">
+
+
+	 
+	
+		<h1>Enter Students Results Here</h1>
+			<form action="check.php">
+				<table class="imagetable">
+					  <tr>
+						<th>Names</th>
+						<th>Maths</th>
+						<th>English</th>
+						<th>Physics</th>
+						<th>Biology</th>
+						<th>Chemistry</th>
+						<th>Computer Studies</th>
+					  </tr>
+					  <tr>
+						<td>Seun Daramola</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+						<td>
+							<input type="number">
+							<select>
+								<option>F</option>
+								<option>E</option>
+								<option>D</option>
+								<option>C</option>
+								<option>B</option>
+								<option>A</option>
+							</select>
+						</td>
+					  </tr>
+					 
+				</table><br>
+
+				<button class="myButton">Submit Student Record</button>
+			</form>
+		<hr>
+			
+		</div>
+</body>
+
+</html> 
