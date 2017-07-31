a1733e39233c4f883652be4169883810ebcc5b40
seunzone <darealseun@gmail.com>
2017-07-31 10:23:51 +0100

    display result page added

---
+++ displayResult.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- CSS FILES -->
+<link rel="stylesheet" href="css/main.css"/>
+
+
+</head>
+
+
+<body>
+<hr>
+	
+	<div class="myDiv" align="center">
+
+
+	 
+	
+		<h1>Students Results Sheet</h1>
+			
+				<table class="imagetable">
+					  <tr>
+						<th>Names</th>
+						<th>Maths</th>
+						<th>English</th>
+						<th>Physics</th>
+						<th>Biology</th>
+						<th>Chemistry</th>
+						<th>Computer Studies</th>
+						<th>Total</th>
+					  </tr>
+					  <tr>
+						<td>Femi Johnson</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+
+					  </tr>
+
+
+					   <tr>
+						<td>Chibuzo Michaels</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+						<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+					  </tr>
+
+
+					   <tr>
+						<td>Musa Philips</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+					  </tr>
+
+					   <tr>
+						<td>Leslie Dzong</td>
+						<td>
+						<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+						<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+						<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+					  </tr>
+
+					   <tr>
+						<td>Oju Ciroma</td>
+						<td>
+						<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+						<td>
+							<h2><span></span></h2>
+						</td>
+					  </tr>
+					 
+				</table><br>
+
+				<button class="myButton">Go back Home</button>
+			
+		<hr>
+			
+		</div>
+</body>
+
+</html> 
